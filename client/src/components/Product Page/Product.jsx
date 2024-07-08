06e3064113884b55e5269e4a4d9bda2b5828d553
utkarshhh17 <singhutkarsh.1210@gmail.com>
2024-07-09 00:46:07 +0530

    push

--- client/src/components/Product Page/Product.jsx
+++ client/src/components/Product Page/Product.jsx
@@ -1,14 +1,17 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 import Nav from "../Nav/Nav";
+import { ProductContext } from "../../context/ProductContext";
 
 export default function Product(){
     const { product } = useParams();
 
     const [responseData, setResponseData]=useState(null);
 
+    const {productData, setProductData}=useContext(ProductContext);
 
+    const [parsedResponse,setParsedResponse]=useState("")
     useEffect(() => {
         const fetchData = async () => {
             const stringurl='http://localhost:8081/api/v1/product?productID='+product;
@@ -18,6 +21,7 @@ export default function Product(){
                 if (response.status === 200) {
                     console.log(json);
                     setResponseData(json);
+                    setParsedResponse(parseMarkdown(json))
     
                 }
                 else{
@@ -46,19 +50,25 @@ export default function Product(){
     return(
         <div className="flex flex-col">
             <div><Nav /></div>
-            {responseData &&
+            {productData &&
                 <div>
                 <div className="flex justify-start small:hidden">
-                    <img src={responseData.imageurl} alt="Product Image" className="h-[35vw] mt-[4vw] ml-10"></img>
+                    <img src={productData.imageUrls[0]} alt="Product Image" className="h-[35vw] mt-[4vw] ml-10"></img>
                     <div className="flex flex-col font-lato justify-start ml-[12vw]">
-                        <div className="text-6xl font-bold mt-[4vw]">{responseData.website}</div>
-                        <div className="text-3xl font-bold mt-[1vw]">{responseData.name}</div>
-                        <div className="text-2xl mt-[0.7vw]">Rs. {responseData.price}</div>
-                        
+                        <div className="text-3xl font-bold mt-[1vw]">{productData.productName}</div>
+                        {responseData && 
+                            <div className="">
+
+                                <h2>Food Analysis</h2> 
+                                <div dangerouslySetInnerHTML={{ __html: parsedResponse }} />
+
+                            </div>
+                        }
                     </div>
 
                 </div>
 
+                
 
                 {/* <div className="large:hidden flex flex-col justify-start items-center">
                     <img src={responseData.imageurl} alt="Product Image" className="small:ml-0 small:h-80 small:w-60 h-[35vw] mt-[4vw] ml-10"></img>
@@ -79,3 +89,9 @@ export default function Product(){
 
 
 }
+
+const parseMarkdown = (text) => {
+    return text
+      .replace(/\*\*(.*?)\*\*/g, '<strong>$1</strong>') // Replace **bold** with <strong>bold</strong>
+      .replace(/\n/g, '<br>'); // Replace new lines with <br>
+  };
--- client/src/components/Products Page/Products.jsx
+++ client/src/components/Products Page/Products.jsx
@@ -1,13 +1,20 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState, useContext } from "react"
 import Nav from "../Nav/Nav"
 import axios from "axios";
-import { Link, useParams } from "react-router-dom";
+import { Link, useParams, useNavigate } from "react-router-dom";
+import { ProductContext } from "../../context/ProductContext";
 export default function Products(){
     const [error, setError] = useState('');
     const [data,setData]=useState([]);
     const { category } = useParams();
 
+    const navigate=useNavigate()
 
+    const {productData, setProductData}=useContext(ProductContext);
+    const handleClick=(product)=>{
+        setProductData(product);
+        navigate(`/products/${product.id}`)
+    }
 
     useEffect(() => {
         const fetchData = async () => {
@@ -58,7 +65,7 @@ export default function Products(){
             
             <div className="flex flex-wrap justify-left small:justify-center mb-2">
                 {data.map((product, index) => (
-                    <Link to={`/products/${product.id}`} className="flex flex-col items-center justify-start small:w-[90vw] small:h-80 small:mb-5 small:ml-0 w-[23vw] h-[25vw] border border-gray-300 rounded-lg ml-6 mt-6 hover:scale-105 cursor-pointer" key={index}>
+                    <div onClick={()=>handleClick(product)} className="flex flex-col items-center justify-start small:w-[90vw] small:h-80 small:mb-5 small:ml-0 w-[23vw] h-[25vw] border border-gray-300 rounded-lg ml-6 mt-6 hover:scale-105 cursor-pointer" key={index}>
                         
                         <div style={{ backgroundImage: `url(${product.imageUrls[0]})` }} className="rounded-lg bg-contain bg-no-repeat bg-center w-full h-full"></div>
 
@@ -66,7 +73,7 @@ export default function Products(){
                         <p className=" text-center font-lato text-md truncate w-full border-b-gray-200 border-b-2">{product.productName}</p>
                         {/* <p className=" text-center font-lato  text-base">Rs. {product.price}</p> */}
                        
-                    </Link>
+                    </div>
                 ))}
             </div>
         </div>
